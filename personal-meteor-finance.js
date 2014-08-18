830fc1aad741e7f8156afb6dccb056d00c0b8b41
usualdesigner <usualdesigner@gmail.com>
2014-08-18 16:23:22 +0300

    added some new code

--- personal-meteor-finance.js
+++ personal-meteor-finance.js
@@ -1,16 +1,46 @@
 if (Meteor.isClient) {
 
-    Template.report.monthName = function (_month) {
+    Template.report.monthName = function (m) {
 
         var monthNames = [ "January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December" ];
 
-        return monthNames[--_month];
+        return monthNames[--m];
     };
 
-    Template.report.month = function () {
-        return this.params._month;
-    };
+//    Template.report.nextLink = function (m, y) {
+//
+//        var month,
+//            year;
+//
+//        if ((++m) == 13) {
+//            month = 1;
+//            year = --y;
+//        }
+//        else {
+//            month = m;
+//            year = y;
+//        }
+//
+//        return '/report/' + year + '/' + month;
+//    };
+//
+//    Template.report.prevLink = function (m, y) {
+//
+//        var month,
+//            year;
+//
+//        if ((--m) == 0) {
+//            month = 12;
+//            year = ++y;
+//        }
+//        else {
+//            month = m;
+//            year = y;
+//        }
+//
+//        return '/report/' + year + '/' + month;
+//    };
 
 }
 
--- router.js
+++ router.js
@@ -1,14 +1,43 @@
-Router.map(function() {
-    this.route('home',{path: '/'});
+Router.map(function () {
+    this.route('home', {path: '/'});
     this.route(
         'report', {
             path: '/report/:_year/:_month',
-            data: function (){
+            data: function () {
+
+                var nextMonth,
+                    nextYear,
+                    prevMonth,
+                    prevYear,
+                    currentMonth = this.params._month,
+                    currentYear = this.params._year;
+
+                if ((parseInt(currentMonth) - 1) == 0) {
+                    prevMonth = 12;
+                    prevYear = parseInt(currentYear) - 1;
+                } else {
+                    prevMonth = parseInt(currentMonth) - 1;
+                    prevYear = currentYear;
+                }
+
+                if ((parseInt(currentMonth) + 1) == 13) {
+                    nextMonth = 1;
+                    nextYear = parseInt(currentYear) + 1;
+                } else {
+                    nextMonth = parseInt(currentMonth) + 1;
+                    nextYear = currentYear;
+                }
+
                 templateData = {
-                    _year: this.params._year,
-                    _month: this.params._month
+                    nextMonth: nextMonth,
+                    nextYear: nextYear,
+                    prevMonth: prevMonth,
+                    prevYear: prevYear,
+                    currentMonth: currentMonth,
+                    currentYear: currentYear
                 };
                 return templateData;
+
             }
         }
     );
