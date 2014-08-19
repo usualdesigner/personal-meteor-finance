b744fa69faac6688e90d938f5f81ce67c2bae684
usualdesigner <usualdesigner@gmail.com>
2014-08-19 13:53:00 +0300

    added aggregated counters

--- personal-meteor-finance.js
+++ personal-meteor-finance.js
@@ -12,7 +12,35 @@ if (Meteor.isClient) {
 
         incoming: function () {
 
-            return Incoming.find({m: parseInt(this.currentMonth), y: parseInt(this.currentYear)});
+            return Incoming.find({m: parseInt(this.currentMonth), y: parseInt(this.currentYear)}, {sort: {d: 1, trend: 1}});
+
+        },
+
+        incoming_expense_sum: function () {
+
+            collection = Incoming.find({m: parseInt(this.currentMonth), y: parseInt(this.currentYear), trend: 'expense'});
+
+            sum = 0;
+
+            collection.forEach(function(element, index, array){
+                sum+= element.value;
+            });
+
+            return sum;
+
+        },
+
+        incoming_earning_sum: function () {
+
+            collection = Incoming.find({m: parseInt(this.currentMonth), y: parseInt(this.currentYear), trend: 'earning'});
+
+            sum = 0;
+
+            collection.forEach(function(element, index, array){
+                sum+= element.value;
+            });
+
+            return sum;
 
         }
 
